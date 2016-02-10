6207b0e5f2d63b43b88562f4317de99f69731f67
ChristenBowies <aimforchris@gmail.com>
2016-02-10 12:30:57 -0500

    Initial submit. Barebone API

---
+++ DashboardClient/DashboardClient/twitchAPI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using RestSharp;
+
+namespace DashboardClient
+{
+    class twitchAPI
+    {
+        public void getUser()
+        {
+            //API endpoint for Twitch
+            string url = @"https://api.twitch.tv/kraken/user";
+
+
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.GET);
+            var response = client.Execute(request);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                
+            }
+            else
+            {
+               
+            }
+        }
+
+        public class User
+        {
+            public string 
+        } 
+    }
+}
